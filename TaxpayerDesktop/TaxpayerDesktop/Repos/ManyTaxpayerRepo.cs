611ea50d903c1a66baf75f6efa830becdc923f0e
2020b Gavlik Martin <144203996+b0gavmar@users.noreply.github.com>
2025-05-15 20:43:25 +0200

    pár viewmodel létrehozva

---
+++ TaxpayerDesktop/TaxpayerDesktop/Repos/ManyTaxpayerRepo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaxpayerConsole.Models;
+
+namespace TaxpayerDesktop.Repos
+{
+    public class ManyTaxpayerRepo
+    {
+        private readonly TaxpayerContext _context = new TaxpayerContext();
+    }
+}
