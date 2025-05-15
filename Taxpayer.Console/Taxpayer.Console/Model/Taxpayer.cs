392a95d4f3e17f4ae95b8b7ce9f886147214af16
2020b Gavlik Martin <144203996+b0gavmar@users.noreply.github.com>
2025-05-15 19:16:35 +0200

    1. rész kész (15p)

---
+++ Taxpayer.Console/Taxpayer.Console/Model/Taxpayer.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaxpayerConsole.Model
+{
+    public class Taxpayer
+    {
+        private int _amount;
+        private string _email;
+
+        public string Name { get; set; }
+        public int Amount { get => _amount; set => _amount = value; }
+        public string Email { get=> _email; set => _email = value; }
+
+        public Taxpayer(string name,  string email, int amount = 0)
+        {
+            if(string.IsNullOrWhiteSpace(name) || string.IsNullOrEmpty(email))
+                throw new ArgumentException("Email és név nem lehet üres vagy null", nameof(name));
+
+            Name = name;
+            _amount = amount;
+            _email = email;
+        }
+
+        public void IncreaseTaxCredit(int amount)
+        {
+            if (amount > 0)
+            {
+                _amount += amount;
+            }
+            else
+            {
+                throw new ArgumentException("Az összeg nem lehet 0 vagy negatív", nameof(amount));
+            }
+        }
+
+        public void DecreaseTaxCredit(int amount)
+        {
+            if (amount > 0)
+            {
+                _amount -= amount;
+            }
+            else
+            {
+                throw new ArgumentException("Az összeg nem lehet 0 vagy negatív", nameof(amount));
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"Név: {Name}, Email: {Email}, Adójóváírás: {Amount} Ft";
+        }
+    }
+}
--- Taxpayer.Console/Taxpayer.Console/Program.cs
+++ Taxpayer.Console/Taxpayer.Console/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using TaxpayerConsole.Model;
+
+try
+{
+    Taxpayer emptymail = new Taxpayer("John Doe", "", 1000);
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"Hiba: {ex.Message}");
+}
+
+Taxpayer antal = new Taxpayer("Adózó Antal", "adozo.antal@ado.hu");
+try
+{
+    antal.IncreaseTaxCredit(-2000);
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"Hiba: {ex.Message}");
+}
+antal.IncreaseTaxCredit(20000);
+antal.DecreaseTaxCredit(10000);
+Console.WriteLine(antal);
