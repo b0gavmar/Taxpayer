46e7499648080de5d9848bf78850880b7485795a
2020b Gavlik Martin <144203996+b0gavmar@users.noreply.github.com>
2025-05-15 20:32:58 +0200

    repo műveletek

--- Taxpayer.Console/Taxpayer.Console/Repos/TaxpayerRepo.cs
+++ Taxpayer.Console/Taxpayer.Console/Repos/TaxpayerRepo.cs
@@ -32,5 +32,55 @@ namespace TaxpayerConsole.Repos
         {
             return _context.Taxpayers.ToList();
         }
+
+        public List<Taxpayer> GetTaxpayersWithMin(int amount)
+        {
+            return _context.Taxpayers.Where(t=> t.Amount>=amount).ToList();
+        }
+
+        public List<Taxpayer> GetTaxpayersDesc()
+        {
+            return _context.Taxpayers.OrderByDescending(t=>t.Amount).ToList();
+        }
+
+        public List<Taxpayer> GetTaxpayersWithDomain(string domain)
+        {
+            return _context.Taxpayers.Where(t=>t.Email.EndsWith(domain)).ToList();
+        }
+
+        public void ChangeAmountByEmail(string email, int newAmount)
+        {
+            Taxpayer taxpayer = _context.Taxpayers.FirstOrDefault(t => t.Email == email);
+            if(taxpayer == null)
+            {
+                throw new ArgumentException("Nincs ilyen email című adózó");
+            }
+            taxpayer.Amount = newAmount;
+            _context.SaveChanges();
+        }
+
+        public void AddTaxpayer(string name,string email, int newAmount)
+        {
+            Taxpayer taxpayer = _context.Taxpayers.FirstOrDefault(t => t.Email == email);
+            if (taxpayer == null)
+            {
+                Taxpayer newTaxpayer = new Taxpayer(name, email, newAmount);
+                _context.Taxpayers.Add(newTaxpayer);
+            }
+        }
+
+        public void DeleteTaxpayer(string email)
+        {
+            Taxpayer taxpayer = _context.Taxpayers.FirstOrDefault(t => t.Email == email);
+            if (taxpayer != null)
+            {
+                _context.Taxpayers.Remove(taxpayer);
+            }
+            else
+            {
+                throw new ArgumentException("Nincs ilyen email című adózó");
+            }
+        }
+
     }
 }
